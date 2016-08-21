77d83cb67f36262d101fbb15b244194d308c4544
MuffinsaursRex <jackyfryers@gmail.com>
2016-08-21 08:37:45 -0700

    move to puiblic, added port

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ var express = require('express'),
     app = express()
     morgan = require('morgan'),
 	bodyParser = require('body-parser'),
-    //port = process.env.PORT || 3000,
+    port = process.env.PORT || 3000,
     publicDir = require('path').join(__dirname, '/public'),
     //socket server to web client
     http = require('http').Server(app),
