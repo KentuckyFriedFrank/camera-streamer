30b394bd58370f9dcfd88f5e9e99c3b2f0e8d905
MuffinsaursRex <jackyfryers@gmail.com>
2016-08-24 18:43:48 -0700

    sdfgsdfg

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ socketServer.on('connection', function(socket) {
 	streamHeader.writeUInt16BE(width, 4);
 	streamHeader.writeUInt16BE(height, 6);
 	socket.send(streamHeader, {binary:true});
-    child = exec("avconv -s " + width + "x" + height + " -r 10 -f video4linux2 -i /dev/video0 -vf hflip -f mpeg1video -b 300k -r 30 http://127.0.0.1:8082/test1234/" + height + "/" + width + "/");
+    child = exec("avconv -s " + width + "x" + height + " -r 10 -f video4linux2 -i /dev/video0 -vf hflip -f mpeg1video -b 300k -r 30 http://127.0.0.1:8082/test1234/" + width + "/" + height + "/");
 	console.log( 'New WebSocket Connection ('+socketServer.clients.length+' total)' );
 	
 	socket.on('close', function(code, message){
